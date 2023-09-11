3949e22de4147b510e57a4cba66350c03d8241e9
HankDeTank05 <henryhankholman@gmail.com>
2023-09-11 15:38:01 -0500

    began creating bsp engine class

--- Raycasting/BinarySpacePartitioning.h
+++ Raycasting/BinarySpacePartitioning.h
@@ -1,13 +1,24 @@
 #ifndef BINARY_SPACE_PARTITIONING_H
 #define BINARY_SPACE_PARTITIONING_H
 
-class BinarySpacePartitioning
+#include "olcPixelGameEngine.h"
+
+#include "SectorMap.h"
+
+class BinarySpacePartitioning : public olc::PixelGameEngine
 {
 public:
 	BinarySpacePartitioning();
 	BinarySpacePartitioning(const BinarySpacePartitioning& bsp) = delete;
 	BinarySpacePartitioning& operator=(const BinarySpacePartitioning& bsp) = delete;
 	virtual ~BinarySpacePartitioning();
+
+public:
+	virtual bool OnUserCreate() override;
+	virtual bool OnUserUpdate(float fElapsedTime) override;
+
+private:
+	SectorMap map;
 };
 
 #endif
